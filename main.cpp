5b47cc4c1d0e1d956e974df20bab50a82e3ed878
Nightlocke <Allison_Moy@baylor.edu>
2012-11-13 17:41:51 -0600

    So. This working?

---
+++ main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <fstream>
+#include <libjpeg>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello, Team Dragon."<<endl<<endl;
+    cout<<"Welcome to the apocalypse."<<endl<<endl;
+    cout<<"They are coming."<<endl<<endl<<endl;
+
+    return 0;
+}
