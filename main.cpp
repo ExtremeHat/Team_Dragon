c4bcc789ba68ed8d4acc17b37aecca1cfb8a8d58
Nightlocke <Allison_Moy@baylor.edu>
2012-11-13 17:39:31 -0600

    Apocalyptic main

---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <fstream>
+#include <libjpeg>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello, Team Dragon."<<endl<<endl;
+    cout<<"Welcome to the apocalypse."<<endl<<endl;
+
+    return 0;
+}
